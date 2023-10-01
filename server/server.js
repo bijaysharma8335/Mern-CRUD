0d9fae68ba296b278c81b19e5baa5c4acc029331
Bijay Sharma <bijaysharma8335@gmail.com>
2023-10-01 16:46:38 +0530

    update routes

--- server/server.js
+++ server/server.js
@@ -17,14 +17,13 @@ mongoose
   .then(() => console.log("connected to mongodb"))
   .catch((err) => console.log(err));
 
-  // const contactRoute = require("./ContactRoute");
 
-  app.use("/contacts", contactRoute);
+
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use("/contacts", contactRoute);
 app.listen(8000, () => {
   console.log("server is running at port", 8000);
 });
